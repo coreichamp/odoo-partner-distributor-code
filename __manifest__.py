26fdea67aa2d15864abd3700b134d1add5323114
Champ <kkw.phattarapon@gmail.com>
2020-01-14 12:50:07 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Partner Distributor Code",
+    'description': 'Add Distributor Code on partner',
+    "category": "Extra Tools",
+    "summary": "Add Distributor Code on partner",
+    'version': '12.0.1.0.0',
+    "author": "CHAMP",
+    "depends": ["base"],
+    "data": ["views/res_partner.xml"],
+    "installable": True,
+    'application': False,
+}
